8b2fdbf1f12cda15600e54b4b6c82fafc98533a1
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2023-02-23 23:18:12 +0530

    Configured store

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.css';
@@ -5,17 +6,20 @@ import Navbar from './components/Navbar/Navbar';
 
 import Cart from './pages/Cart/Cart';
 import Home from './pages/Home/Home';
+import store from './store/store';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route exact path='/' element={<Home />}></Route>
-          <Route exact path='/cart' element={<Cart />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route exact path='/' element={<Home />}></Route>
+            <Route exact path='/cart' element={<Cart />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
