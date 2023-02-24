8207412da3017c946c54727e8a79141594da84f3
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2023-02-24 08:05:48 +0530

    Added add to cart button and implemented it' functionality

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,20 +1,29 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { add } from '../../store/cartSlice';
 
 import './Product.css';
 
 const Product = (props) => {
+  const dispatch = useDispatch();
   const {item} = props;
   console.log(item)
+  const addToCart = (product) => {
+    dispatch(add(product));
+  }
   return (
     <div className='card'>
       <div className='cardImage'>
         <img src={item.image} alt={item.title}></img>
       </div>
-      <div className='cardBody'>
+      <div className='cardContent'>
         <h6 className='rating flex align-items-center justify-content-center'>Rating: {item.rating.rate} | {item.rating.count}</h6>
         <div className='card-title-price flex justify-content-between'>
-          <h5 className='cardTitle'>{item.title}</h5>
+          <h5 className='cardTitle'>{item.title.length > 30 ? `${item.title.substring(0, 30)}...` : item.title}</h5>
           <h5 className='cardPrice'>{item.price}</h5>
+        </div>
+        <div className='cardButton'>
+          <button className='btn btn-primary' onClick={() => addToCart(item)}>Add to Cart</button>
         </div>
       </div>
     </div>
