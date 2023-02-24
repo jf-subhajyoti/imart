bec53505a40342d36772fd05af764591e4fcbc37
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2023-02-24 08:07:47 +0530

    Displaying cart count dynamically

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import './Navbar.css';
 
 const Navbar = () => {
+  const products = useSelector((state) => state.cart)
   return (
     <nav className='navbar flex justify-content-between align-items-center'>
         <Link to="/" className='logo'>iMart</Link>
         <div className='links flex justify-content-end'>
             <Link className='navLink' to="/">Home</Link>
-            <Link className='navLink' to="/cart">Cart</Link>
+            <Link className='navLink' to="/cart">
+              Cart
+              <span className='badge'>{products.length}</span>
+            </Link>
         </div>
     </nav>
   )
