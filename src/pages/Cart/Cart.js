571785d2c7269c196e40861cc6b2f88b3bbe6343
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2023-03-01 00:01:17 +0530

    Added remove functionality in cart

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,21 +1,39 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { remove } from '../../store/cartSlice';
+
+import './Cart.css';
 
 const Cart = () => {
   const products = useSelector(state => state.cart);
+  const dispatch = useDispatch();
+
+  const removeProductFromCart = (productId) => {
+    dispatch(remove(productId));
+  }
   return (
-    <>
+    <div className='cart_container'>
       {
         products.map(product => {
           return (
-            <>
-              {product.title}
-              {product.price}
-            </>
+            <div key={product.id} className='cart_card flex'>
+              <div className='card_image'>
+                <img src={product.image} alt={product.title}></img>
+              </div>
+              <div className='card_content flex flex-direction-column'>
+                <p>{product.title}</p>
+                <h5>&#8377; {product.price}</h5>                
+              </div>
+              <i onClick={() => removeProductFromCart(product.id)} className='cross_icon pointer'>
+                <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M19 5L4.99998 19M5.00001 5L19 19" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                </svg></i>
+            </div>
           )
         })
       }
-    </>
+    </div>
   )
 }
 
