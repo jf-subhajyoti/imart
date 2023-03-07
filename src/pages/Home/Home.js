f13a7c948434966829b74a483bebcae86c9c16a2
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2023-03-07 08:22:02 +0530

    Used product from store

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,29 +1,43 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
 import Product from '../../components/Product/Product';
 
 import './Home.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchProducts, STATUSES } from '../../store/productSlice';
 
 const Home = () => {
-    const BASE_URL = 'https://fakestoreapi.com/';
-    const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const {data: products, status} = useSelector(state => state.product)
+  // const BASE_URL = 'https://fakestoreapi.com/';
+  // const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        getProducts('products')
-        .then(res => {
-            setProducts(res);
-        }).catch((err) => {
-            console.log(err);
-        })
-    }, []);
+  useEffect(() => {
+    dispatch(fetchProducts());
+      // getProducts('products')
+      // .then(res => {
+      //     setProducts(res);
+      // }).catch((err) => {
+      //     console.log(err);
+      // })
+  }, []);
 
-    const getProducts = async (endPoint = '') => {
-        let res = await axios.get(`${BASE_URL}${endPoint}`, {
-            params: {}
-        });
-        return res.data;
-    }
+  // const getProducts = async (endPoint = '') => {
+  //     let res = await axios.get(`${BASE_URL}${endPoint}`, {
+  //         params: {}
+  //     });
+  //     return res.data;
+  // }
+
+  if(status === STATUSES.LOADING) {
+    return <h2>Loading......</h2>
+  }
+
+  if(status === STATUSES.ERROR) {
+    return <h2>Something Went Wrong!!</h2>
+  }
+
   return (
     <div>
       <h1 className='primary_heading'>Welcome to iMart</h1>
